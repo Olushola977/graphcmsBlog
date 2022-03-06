9c5ab653dc92a2867a2d7f4ce7b7c725d26402e7
adeyeye shola <adeyeyeolushola977@gmail.com>
2022-03-06 23:52:33 +0100

    first commit

--- components/FeaturedPost.jsx
+++ components/FeaturedPost.jsx
@@ -1,56 +1,29 @@
 import React from 'react';
-import Slider from "react-slick";
+import moment from 'moment';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const FeaturedPost = ({ posts }) => {
+const FeaturedPost = ({ post }) => (
+  <div className="relative h-72">
+    <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72" style={{ backgroundImage: `url('${post.featured.url}')` }} />
+    <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72" />
+    <div className="flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full">
+      <p className="text-white mb-4 text-shadow font-semibold text-xs">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
+      <p className="text-white mb-4 text-shadow font-semibold text-2xl text-center">{post.t}</p>
+      <div className="flex items-center absolute bottom-5 w-full justify-center">
+        <Image
+          unoptimized
+          alt={post.author.name}
+          height="30px"
+          width="30px"
+          className="align-middle drop-shadow-lg rounded-full"
+          src={post.author.photo.url}
+        />
+        <p className="inline align-middle text-white text-shadow ml-2 font-medium">{post.author.name}</p>
+      </div>
+    </div>
+    <Link href={`/post/${post.slug}`}><span className="cursor-pointer absolute w-full h-full" /></Link>
+  </div>
+);
 
-    const lgsettings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        centerMode: true,
-        arrows: true
-    };
-
-    const smsettings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    };
-
-    return (
-        <>
-        <div className='mb-10 lgslide'>
-            <Slider {...lgsettings} className='w-full'>
-                {posts && posts.map((post, index) => (
-                    <div key={index} style={{ height: '30rem' }} className='mr-5'>
-                        <img alt='Post Image' src={post.featured.url} className='h-50' />
-                        <div className='bg-white p-2'>
-                            <img alt='Author Image' src={post.author.photo.url} className='w-10 h-10 rounded-full object-cover' />
-                            <p> {post.author.name} </p>
-                        </div>
-                    </div>
-                ))}
-            </Slider>
-        </div>
-        <div className='mb-5 smslide'>
-        <Slider {...smsettings} className='w-full gap-x-3'>
-                {posts && posts.map((post, index) => (
-                    <div key={index} style={{ height: '30rem' }}>
-                        <img alt='Post Image' src={post.featured.url} className='h-50' />
-                        <div className='bg-white p-2'>
-                            <img alt='Author Image' src={post.author.photo.url} className='w-10 h-10 rounded-full object-cover' />
-                            <p> {post.author.name} </p>
-                        </div>
-                    </div>
-                ))}
-            </Slider>
-        </div>
-        </>
-    )
-}
-
-export default FeaturedPost
+export default FeaturedPost;
